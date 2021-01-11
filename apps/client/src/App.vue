4a39774c47884cd4145d1d19e8dca8ef932c3e99
equaliser0 <accounts@equaliser.minimalcity.de>
2021-01-11 19:54:34 +0100

    Add Playground Client for Prototyping

---
+++ apps/client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { Session } from '@scale/session'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      session: null
+    }
+  },
+  mounted() {
+    this.session = new Session({
+      host: 'ws://localhost:9090',
+      instance: 'dev',
+      gateway: 'playground'
+    })
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
