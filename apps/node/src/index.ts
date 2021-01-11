f45a60a8b1fc22fb2ad721434668ff2dcad944ad
equaliser0 <accounts@equaliser.minimalcity.de>
2021-01-11 19:55:12 +0100

    Update Node Usage Example

--- apps/node/src/index.ts
+++ apps/node/src/index.ts
@@ -1,12 +1,17 @@
-import { Host, Gateway, Endpoint } from '@scale/core';
+import { Host, Instance, Gateway, Endpoint } from '@scale/core';
 
-const host = new Host({})
+const host = new Host({
+  cors: { origin: '*', credentials: false },
+  transports: ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling']
+})
 
 host.add([
-  new Gateway('dev', [
-    new Endpoint('check', (id: any, data: any) => {
-      console.log(id, data);
-    })
+  new Instance('dev', [
+    new Gateway('playground', [
+      new Endpoint('check', (id: any, data: any) => {
+        console.log(id, data);
+      })
+    ])
   ])
 ])
 
