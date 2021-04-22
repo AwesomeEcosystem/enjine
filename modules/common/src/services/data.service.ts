134c8b3de0f03811a4dc2b55b4977224148aa259
equaliser0 <accounts@equaliser.minimalcity.de>
2021-04-22 13:46:37 +0200

    switch filesystem

---
+++ modules/common/src/services/data.service.ts
@@ -0,0 +1,23 @@
+export class DataService {
+  public async all(params: any) {
+
+  }
+  public async post(params: any) {
+
+  }
+  public async put(params: any) {
+
+  }
+  public async get(params: any) {
+
+  }
+  public async find(params: any) {
+
+  }
+  public async filter(params: any) {
+
+  }
+  public async remove(params: any) {
+
+  }
+}
