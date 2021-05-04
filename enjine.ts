6cfe66285b070295f22cf7eb6330512373091800
equaliser0 <accounts@equaliser.minimalcity.de>
2021-05-04 11:42:29 +0200

    prototype

---
+++ enjine.ts
@@ -0,0 +1,6 @@
+import { Application } from '@enjine/common';
+
+export default new Application({
+  theme: '@enjine/components/themes/basic', // css file or tailwind.config.js
+  components: [ '@enjine/components' ]
+})
