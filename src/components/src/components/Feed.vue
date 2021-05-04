9e6a333ce70d63d475a488c4d2987b8dc830209d
equaliser0 <accounts@equaliser.minimalcity.de>
2021-05-04 11:05:35 +0200

    rename prop

--- src/components/src/components/Feed.vue
+++ src/components/src/components/Feed.vue
@@ -3,7 +3,7 @@
     <slot name="top"></slot>
 
     <div class="flex flex-wrap">
-      <div class="" v-if="feed" v-for="(doc, i) in feed">
+      <div class="" v-if="gateway" v-for="(doc, i) in data">
         <div class="p-2">
           <slot name="doc">{{ doc }}</slot>
         </div>
@@ -22,17 +22,17 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Feed extends Vue {
-  @Prop() feed!: any;
+  @Prop() gateway!: any;
 
   data: any[] = [];
 
   mounted() {
 
-    this.feed.emit('all', (arr: any[]) => this.data = arr);
+    this.gateway.emit('all', (arr: any[]) => this.data = arr);
 
-    this.feed.on('post', (obj: any[]) => this.data.post(obj));
-    this.feed.on('update', (obj: any[]) => this.data.update(obj));
-    this.feed.on('remove', (obj: any[]) => this.data.remove(obj));
+    this.gateway.on('post', (obj: any[]) => this.data.post(obj));
+    this.gateway.on('update', (obj: any[]) => this.data.update(obj));
+    this.gateway.on('remove', (obj: any[]) => this.data.remove(obj));
   }
 }
 
--- src/components/src/components/Stream.vue
+++ src/components/src/components/Stream.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-wrap bg-gray-700 py-4 px-8 m-2 rounded text-white">
-    <Feed :feed="session.gateway.data" v-slot="{ doc }">
+    <Feed :gateway="session.gateway.data" v-slot="{ doc }">
       <Card>{{ doc }}</Card>
     </Feed>
   </div>
