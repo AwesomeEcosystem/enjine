e09a8ebc522aa80530a264b096beafb7aee29fe7
equaliser0 <accounts@equaliser.minimalcity.de>
2021-07-09 17:49:27 +0200

    Remove User Gateway

--- packages/common/src/gateways/user/user.gateway.ts
+++
@@ -1,8 +0,0 @@
-import { Gateway, authMiddleware } from '@enjine/core';
-import { endpoints } from './user.endpoints';
-
-export class UserGateway extends Gateway {
-  constructor(config: any) {
-    super(config.name, endpoints, config.db)
-  }
-}
