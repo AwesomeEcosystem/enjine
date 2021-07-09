4edc8f08b4e655247a515cfcf0cbd6e3eecc8de0
equaliser0 <accounts@equaliser.minimalcity.de>
2021-07-09 17:51:50 +0200

    update

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -5,6 +5,5 @@ export * from './controllers/data/data.controller'
 export * from './gateways/auth/auth.gateway'
 export * from './gateways/data/data.gateway'
 export * from './gateways/media/media.gateway'
-export * from './gateways/user/user.gateway'
 
 export * from './models/document.model'
