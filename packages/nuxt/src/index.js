9b89cad4d52444ffddf046b27803ffe7a32ab085
equaliser0 <accounts@equaliser.minimalcity.de>
2021-08-05 15:49:33 +0200

    update

--- packages/nuxt/src/index.js
+++ packages/nuxt/src/index.js
@@ -11,58 +11,6 @@ function themeModule () {
   const { nuxt } = this
   const { options, hook } = this.nuxt
 
-  // Configure `content/` dir
-  options.content.dir = path.resolve(options.rootDir, options.content.dir || 'content')
-  // Configure `static/ dir
-  options.dir.static = path.resolve(options.rootDir, options.dir.static || 'static')
-  // Configure `components/` dir
-  hook('components:dirs', async (dirs) => {
-    const componentsDirPath = path.resolve(nuxt.options.rootDir, 'components')
-    const componentsDirStat = await fs.stat(componentsDirPath).catch(() => null)
-    if (componentsDirStat && componentsDirStat.isDirectory()) {
-      dirs.push({
-        path: componentsDirPath
-      })
-    } else {
-      nuxt.options.watch.push(componentsDirPath)
-    }
-
-    const globalComponentsDirPath = path.resolve(nuxt.options.rootDir, 'components/global')
-    const globalComponentsDirStat = await fs.stat(globalComponentsDirPath).catch(() => null)
-    if (globalComponentsDirStat && globalComponentsDirStat.isDirectory()) {
-      dirs.push({
-        path: globalComponentsDirPath,
-        global: true
-      })
-    } else {
-      nuxt.options.watch.push(globalComponentsDirPath)
-    }
-  })
-  // Configure content after each hook
-  hook('content:file:beforeInsert', (document) => {
-    const regexp = new RegExp(`^/(${options.i18n.locales.map(locale => locale.code).join('|')})`, 'gi')
-    const { dir, slug, category } = document
-    const _dir = dir.replace(regexp, '')
-    const _slug = slug.replace(/^index/, '')
-    const _category = category && typeof category === 'string' ? category : ''
-
-    document.to = `${_dir}/${_slug}`
-    document.category = _category
-  })
-  // Extend `/` route
-  hook('build:extendRoutes', (routes) => {
-    const allRoute = routes.find(route => route.name === 'all')
-
-    routes.push({
-      ...allRoute,
-      path: '/',
-      name: 'index'
-    })
-  })
-  // Override editor style on dev mode
-  if (options.dev) {
-    options.css.push(path.resolve(__dirname, 'assets/css/main.dev.css'))
-  }
   // Configure `tailwind.config.js` path
   options.tailwindcss.configPath = options.tailwindcss.configPath || path.resolve(options.rootDir, 'tailwind.config.js')
   options.tailwindcss.cssPath = options.tailwindcss.cssPath || path.resolve(options.rootDir, options.dir.assets, 'css', 'tailwind.css')
@@ -113,21 +61,11 @@ const defaultConfig = docsOptions => ({
     'nuxt-i18n',
     '@nuxt/content'
   ],
-  components: [
-    { path: '~/components', pathPrefix: false }
-  ],
   loading: {
     color: docsOptions.primaryColor
   },
   meta: {
     theme_color: docsOptions.primaryColor
-  },
-  content: {
-    markdown: {
-      prism: {
-        theme: 'prism-themes/themes/prism-material-oceanic.css'
-      }
-    }
   },
   i18n: {
     locales: [{
