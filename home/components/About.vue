9e0006ae08caa13e1351b6838564c4e0a5bd1f2e
equaliser0 <accounts@equaliser.minimalcity.de>
2021-09-28 18:03:18 +0200

    Add Homepage

---
+++ home/components/About.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="w-full bg-gray-900 text-white text-center">
+    <div class="container mx-auto w-full p-4">
+
+      <div class="py-4 my-8">
+        <div class="w-full text-center py-4 mb-8">
+          <h1>About</h1>
+          <p>A Progressive Full Stack Application SDK</p>
+        </div>
+        <div class="flex flex-wrap justify-around items-center">
+
+          <div class="">
+
+          </div>
+
+        </div>
+      </div>
+
+      <!-- <div class="py-4 my-8">
+        <div class="w-full text-center">
+          <h1>Vision</h1>
+        </div>
+        <div class="flex flex-wrap justify-around">
+
+          <div class="flex flex-col justify-around items-center md:w-1/5 m-2 p-2">
+            <img src="~/assets/imgs/finance.png" class="w-32">
+            <h2>Passive Liquidity</h2>
+            <p></p>
+          </div>
+
+          <div class="flex flex-col justify-around items-center md:w-1/5 m-2 p-2">
+            <img src="~/assets/imgs/security.png" class="w-32">
+            <h2>Entity Insurance</h2>
+            <p></p>
+          </div>
+
+          <div class="flex flex-col justify-around items-center md:w-1/5 m-2 p-2">
+            <img src="~/assets/imgs/business.png" class="w-32">
+            <h2>Community Enterprise</h2>
+            <p></p>
+          </div>
+
+          <div class="flex flex-col justify-around items-center md:w-1/5 m-2 p-2">
+            <img src="~/assets/imgs/monitoring.png" class="w-32">
+            <h2>User Adoption</h2>
+            <p></p>
+          </div>
+
+        </div>
+      </div> -->
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
