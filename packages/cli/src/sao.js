2a04b5f78c3c7fa9f96c03f6d6879a5a2be87831
equaliser0 <accounts@equaliser.minimalcity.de>
2021-09-29 15:56:20 +0200

    Refactore

---
+++ packages/cli/src/sao.js
@@ -0,0 +1,107 @@
+const { random: superb } = require('superb')
+const glob = require('glob')
+const { join } = require('path')
+const validate = require('validate-npm-package-name')
+
+const rootDir = __dirname
+
+const move = (from, to = '') => {
+  const result = {}
+  const options = { cwd: join(rootDir, 'template'), nodir: true, dot: true }
+  for (const file of glob.sync(`${from}/**`, options)) {
+    result[file] = (to ? to + '/' : '') + file.replace(`${from}/`, '')
+  }
+  return result
+}
+
+const moveLanguage = (answer, to = '') => {
+  console.log(`${answer}`)
+  return answer !== 'none' && move(`${answer}`, to)
+}
+
+module.exports = {
+  prompts: {
+    name: {
+      message: 'Project name',
+      default: ':folderName:'
+    },
+    description: {
+      message: 'Project description',
+      default: `${superb()} enjine project`
+    },
+    type: {
+      message: 'Choose Project Type',
+      type: 'list',
+      choices: ['monorepo'],
+      default: 'monorepo'
+    },
+    author: {
+      type: 'string',
+      message: 'Author name',
+      default: ':gitUser:',
+      store: true
+    },
+    pm: {
+      message: 'Choose a package manager',
+      choices: ['npm', 'yarn'],
+      type: 'list',
+      default: 'npm'
+    }
+  },
+  filters: {
+    'monorepo/**': 'type === "monorepo"'
+  },
+  move(answers) {
+    const validation = validate(answers.name)
+    validation.warnings &&
+      validation.warnings.forEach(warn => {
+        console.warn('Warning:', warn)
+      })
+    validation.errors &&
+      validation.errors.forEach(err => {
+        console.error('Error:', err)
+      })
+    validation.errors && validation.errors.length && process.exit(1)
+    return Object.assign(moveLanguage(answers.language))
+  },
+  post(
+    {
+      npmInstall,
+      yarnInstall,
+      gitInit,
+      chalk,
+      isNewFolder,
+      folderName,
+      folderPath
+    },
+    { meta }
+  ) {
+    gitInit()
+
+    // using yarn or npm
+    meta.answers.pm === 'yarn' ? yarnInstall() : npmInstall()
+
+    const cd = () => {
+      if (isNewFolder) {
+        console.log(`\t${chalk.cyan('cd')} ${folderName}`)
+      }
+    }
+
+    cd()
+    console.log()
+    console.log(chalk.bold(`\tTo get started:\n`))
+    cd()
+    console.log(`\t ${meta.answers.pm} run dev\n`)
+    cd()
+    // if (meta.answers.language === 'typescript') {
+    //   console.log(chalk.bold(`  To build TypeScript code:\n`))
+    //   console.log(`\t ${meta.answers.pm} run build`)
+    //   console.log(`\t ${meta.answers.pm} run watch # watch mode`)
+    // }
+
+    console.log(chalk.bold(`\n  To test:\n`))
+    cd()
+    console.log(`\t ${meta.answers.pm} run test`)
+    console.log()
+  }
+}
---
+++ packages/cli/template/monorepo/app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Container>
+    <!-- Code here -->
+  </Container>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+  layout: 'app',
+  mounted() {
+    // console.log(this.$coon);
+    // if (!this.$coon.accounts[0]) {
+    //   this.$router.push('start')
+    // }
+  }
+}
+</script>
