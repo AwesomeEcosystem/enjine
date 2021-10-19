fb7ae48b2be402258ec9ad46a543f2776de29ac2
equaliser0 <accounts@equaliser.minimalcity.de>
2021-10-19 14:50:45 +0200

    refactor login and registration

--- packages/cli/src/template/app/pages/start.vue
+++ packages/cli/src/template/app/pages/start.vue
@@ -1,107 +1,90 @@
 <template>
-  <Container>
+  <App class="flex flex-wrap bg-gray-800">
+    <Container class="bg-gray-900">
+      <div class="flex flex-col justify-around items-center py-8">
+        <img
+          class="w-32 rounded-full my-4"
+          src="~/assets/logos/logo.png"
+          alt=""/>
+        <h1 class="text-5xl font-thin p-2"><%= name %></h1>
+      </div>
 
-    <div class="flex flex-col justify-around items-center">
-      <img
-        class="w-24 rounded-full"
-        src="https://media-exp1.licdn.com/dms/image/C4D0BAQGWmKb5bGho8Q/company-logo_200_200/0/1630482349668?e=1639612800&v=beta&t=c_tMpCx47EqWxWb8wQEmNDS0uvqaeZ0nYd3Ks-P8sEo"
-        alt=""/>
-      <h1 class="text-6xl p-2"><%= name %></h1>
-    </div>
-
-    <div class="flex flex-wrap">
-      <Card class="flex flex-col h-32 p-4" v-if="!created">
-        <p>Create or Import an Account</p>
-        <button class="bg-green-500 py-2 px-4 m-2 rounded font-bold" @click="getStarted()">Get Started</button>
-      </Card>
-      <Card class="flex flex-wrap md:w-1/4 h-auto py-2  " v-else>
-        <input class="w-full bg-gray-600 py-2 p-4 m-2 rounded" v-model="password" placeholder="Enter your Password">
-        <button class="w-full bg-green-500 py-2 p-4 m-2 rounded font-bold" @click="create()">Login</button>
-      </Card>
-    </div>
-
-    <Card class="flex flex-wrap justify-around items-center">
-      <div class="hidden md:block flex-1" v-if="metamask">
-        <button class="flex flex-wrap justify-around items-center w-32 h-32 bg-gray-500 py-2 p-4 m-2 rounded font-bold" @click="metamask()">
-            <img class="w-full" src="https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg" alt="Metamask">
-            Metamask
+      <div class="flex flex-wrap justify-around items-center">
+        <button class="flex flex-wrap justify-around items-center text-center bg-gray-800 hover:bg-gray-700 h-32 p-4 rounded-lg" v-if="!manage" @click="getStarted()">
+          <p class="w-full">Login or Register</p>
+          <div class="py-2 px-4 mb-2 rounded">
+            <h1 class="text-6xl font-thin">+</h1>
+          </div>
         </button>
       </div>
-      <div class="flex flex-wrap justify-around items-center w-full md:flex-1 m-2 p-2">
-        <button class="w-full bg-blue-500 py-2 p-4 m-2 rounded font-bold" @click="mod('login')">Login</button>
-        <button class="w-full bg-blue-500 py-2 p-4 m-2 rounded font-bold" @click="mod('create')">Create</button>
+      <div class="flex flex-wrap justify-around items-center" v-if="manage">
+        <section class="w-full md:w-1/3 bg-gray-800 shadow-inner rounded-xl p-2 m-4">
+          <div class="flex flex-wrap w-full" @submit="(manage === 'login') ? login() : register()">
+            <input class="w-full bg-transparent p-4 active:border-b border-gray-600" v-model="username" placeholder="Username">
+            <input class="w-full bg-transparent p-4 active:border-b border-gray-600" v-model="mail" v-if="manage === 'register'" placeholder="E-Mail">
+            <input class="w-full bg-transparent p-4 active:border-b border-gray-600" v-model="password" placeholder="Password" type="password">
+            <input class="w-full bg-transparent p-4 active:border-b border-gray-600" v-model="password2" v-if="manage === 'register'" placeholder="Confirm Password" type="password">
+            <button class="w-full bg-green-600 hover:bg-green-500 rounded-xl p-2 m-2" v-if="full()" @click="(manage === 'login') ? login() : register()">
+              {{ capitalizeFirstLetter(manage) }}
+            </button>
+          </div>
+        </section>
+        <button class="w-full text-center" @click="change()">
+          {{ (manage === 'login') ? 'Register instead' : 'Login instead' }}
+        </button>
       </div>
-      <div class="" v-if="mode === 'login'">
-        <input v-model="username" placeholder="Username"/>
-        <input v-model="password" placeholder="Password"/>
-        <button class="bg-green-500 py-2 p-4 m-2 rounded font-bold">Login</button>
-      </div>
-      <div class="" v-if="mode === 'create'">
-        <input v-model="username" placeholder="Username"/>
-        <input v-model="mail" placeholder="E-Mail"/>
-        <input v-model="password" placeholder="Password"/>
-        <button class="bg-green-500 py-2 p-4 m-2 rounded font-bold">Create</button>
-      </div>
-      <div class="">
-      </div>
-    </Card>
-
-  </Container>
+    </Container>
+  </App>
 </template>
 
 <script>
-import Vue from 'vue'
-
 export default {
   data() {
     return {
-      username:'',
-      mail:'',
-      password:'',
-      created: false,
-      metamask: false,
-      mode: '',
-    }
-  },
-  async beforeMount() {
-    console.log(this.$account);
-    // await this.$coon.loadAccounts()
-    // // if (this.$coon.accounts[0]) {
-    // //   this.created = true;
-    // // }
-  },
-  mounted() {
-    if (window.ethereum) {
-      this.metamask = true
+      manage: '',
+      username: '',
+      mail: '',
+      password: '',
+      password2: ''
     }
   },
   methods: {
-    mod(str) {
-      this.mode = str;
-    },
-    async getStarted() {
+    async register() {
+        try {
+          if (this.password !== this.password2) {
+            throw new Error('Passwords are not identical!')
+          }
 
-    },
-    async create() {
-      // if (this.creds === this.password) { // Temporaly
-      //   await this.$coon.createAccount()
-      //   await this.$coon.loadAccounts()
-      // }
-    },
+          // await Register Logic here
 
+          this.$toast.success('Registered!')
+          this.change()
+
+        } catch (error) {
+          this.$toast.error(error.message);
+        }
+    },
     async login() {
-      // if (this.creds === this.password) { // Temporaly
-      //   await this.$coon.createAccount()
-      //   await this.$coon.loadAccounts()
-      // }
-      this.$router.push('/')
+      const user = true // Login Logic here
+
+      if (user) {
+        this.$router.push('/')
+      }
     },
-    metamask() {
-      if (this.$moralis) {
-        this.$moralis.authenticate().then((user) => {
-          this.$moralis.accounts = user.attributes.accounts
-          this.accounts = user.attributes.accounts
-        })
+    getStarted() {
+      this.manage = 'login';
+    },
+    change() {
+      (this.manage === 'login') ? this.manage = 'register' : this.manage = 'login';
+    },
+    capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    full() {
+      if (this.manage === 'login' && this.username && this.password) {
+        return true
+      } else if (this.manage === 'register' && this.username && this.mail && this.password && this.password2) {
+        return true
       }
     }
   }
