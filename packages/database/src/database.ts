f7e3c0ca0611255f22bc4e8461b360e93950065c
equaliser0 <accounts@equaliser.minimalcity.de>
2023-01-26 20:36:10 +0100

    restructure

--- packages/database/src/database.ts
+++ packages/database/src/database.ts
@@ -1,23 +1,28 @@
 import Levelts from 'level-ts';
+import * as encrypt from 'crypto';
 import { mkdir } from '@enjine/utils'
 
 export class Database extends Levelts {
-  name: string;
+  name: string
+  encryption: false
 
-  constructor(name: string) {
+  constructor(name: string, encrypt?: boolean) {
     mkdir(name)
     super(name)
-    this.name = name;
+    this.name = name
+    if (encrypt) {
+      this.encryption = true
+    }
   }
   async fetch(): Promise<any> {
-    return await this.all();
+    return await this.all()
   }
 
   async post(data: any): Promise<any> {
-    return await this.put(data._id, data);
+    return await this.put(data._id, data)
   }
 
   async remove(id: string) {
-    return await this.del(id);
+    return await this.del(id)
   }
 }
