853ba99366f3d1c667b3f0784420a2c62f728de3
equaliser0 <accounts@equaliser.minimalcity.de>
2023-01-27 11:05:58 +0100

    fix export import

--- packages/utils/src/lib/index.ts
+++ packages/utils/src/lib/index.ts
@@ -1,18 +1,5 @@
-import { createCrypto, compareCrypto, encrypt, decrypt } from './crypto';
-import { serialize, deserialize, generateKey } from './helpers'
-import { isEmptyObject } from './isEmptyObject';
-import { validateEnv } from './validateEnv';
-import { mkdir } from './dirManagement'
-
-export default class Utils {
-  public createCrypto = createCrypto;
-  public compareCrypto = compareCrypto;
-  public encrypt = encrypt;
-  public decrypt = decrypt;
-  public serialize = serialize;
-  public deserialize = deserialize;
-  public generateKey = generateKey;
-  public isEmptyObject = isEmptyObject;
-  public validateEnv = validateEnv;
-  public mkdir = mkdir;
-}
+export * from './crypto';
+export * from './helpers'
+export * from './isEmptyObject';
+export * from './validateEnv';
+export * from './dirManagement'
